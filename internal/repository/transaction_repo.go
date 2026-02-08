85a0e15ea35ee1a83c65f9ffa3786ab33ae45f57
khazeez <cakirul211001@gmail.com>
2026-02-08 23:44:42 +0700

    update

--- internal/repository/transaction_repo.go
+++ internal/repository/transaction_repo.go
@@ -30,7 +30,7 @@ func (repo *TransactionRepository) CreateTransaction(items []models.CheckoutItem
 		var productPrice, stock int
 		var productName string
 
-		err := tx.QueryRow("SELECT name, price, stock FROM products WHERE id = $1", item.ProductID).Scan(&productName, &productPrice, &stock)
+		err := tx.QueryRow("SELECT product_name, price, stock FROM product WHERE id = $1", item.ProductID).Scan(&productName, &productPrice, &stock)
 		if err == sql.ErrNoRows {
 			return nil, fmt.Errorf("product id %d not found", item.ProductID)
 		}
@@ -41,7 +41,7 @@ func (repo *TransactionRepository) CreateTransaction(items []models.CheckoutItem
 		subtotal := productPrice * item.Quantity
 		totalAmount += subtotal
 
-		_, err = tx.Exec("UPDATE products SET stock = stock - $1 WHERE id = $2", item.Quantity, item.ProductID)
+		_, err = tx.Exec("UPDATE product SET stock = stock - $1 WHERE id = $2", item.Quantity, item.ProductID)
 		if err != nil {
 			return nil, err
 		}
@@ -55,14 +55,14 @@ func (repo *TransactionRepository) CreateTransaction(items []models.CheckoutItem
 	}
 
 	var transactionID int
-	err = tx.QueryRow("INSERT INTO transactions (total_amount) VALUES ($1) RETURNING id", totalAmount).Scan(&transactionID)
+	err = tx.QueryRow("INSERT INTO transaction (total_amount) VALUES ($1) RETURNING id", totalAmount).Scan(&transactionID)
 	if err != nil {
 		return nil, err
 	}
 
 	for i := range details {
 		details[i].TransactionID = transactionID
-		_, err = tx.Exec("INSERT INTO transaction_details (transaction_id, product_id, quantity, subtotal) VALUES ($1, $2, $3, $4)",
+		_, err = tx.Exec("INSERT INTO transaction_details (transaction_id, product_id, quantity, sub_total) VALUES ($1, $2, $3, $4)",
 			transactionID, details[i].ProductID, details[i].Quantity, details[i].Subtotal)
 		if err != nil {
 			return nil, err
